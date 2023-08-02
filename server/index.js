9d5bcc39b50b62792c1bb18c4db14d13481fa427
Jacquiiiii <koroll.jacqui@gmail.com>
2023-08-01 22:26:19 -0700

    feat: update build process and update server method to serve static files

--- server/index.js
+++ server/index.js
@@ -4,30 +4,37 @@ const app = express()
 const db = require('./models')
 const path = require('path')
 const port = process.env.PORT || 54321
+const BUILD_PATH = path.resolve('./public')
 
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(express.static(BUILD_PATH))
 
-db.sequelize.sync().then(function() {
+db.sequelize.sync().then(function () {
   console.log('connected to database')
-}).catch(function(err) {
+}).catch(function (err) {
   console.log(err)
-});
+})
 
-app.get('/', (req, res) => console.log('success'))
+app.get('/', (req, res) => {
+  filePath = BUILD_PATH + '/index.html'
+
+  if (fs.existsSync(filePath)) {
+    res.sendFile(filePath)
+  }
+  else {
+    res.statusCode = 404
+    res.write('404 not found')
+    res.end()
+  }
+})
 
 const productRoutes = require('./routes/productRoutes')
 const userRoutes = require('./routes/userRoutes')
 
 app.use('/products', productRoutes)
 app.use('/users', userRoutes)
-
-app.use(express.static(path.join(__dirname, '../build')))
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build/index.html'))
-})
 
 app.listen(port, () => console.log(`app is listening on port ${port}`))
 
