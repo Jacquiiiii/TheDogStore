530f38faf1698f9e8a9f698493b7116806741118
Jacquiiiii <koroll.jacqui@gmail.com>
2023-08-02 07:43:03 -0700

    fix: rm copy build process

--- server/index.js
+++ server/index.js
@@ -2,33 +2,17 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const db = require('./models')
-const path = require('path')
-const fs = require('fs')
 const port = process.env.PORT || 54321
-const BUILD_PATH = path.resolve('./public')
 
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(express.static(BUILD_PATH))
+app.use(express.static('build'))
 
 db.sequelize.sync().then(function () {
   console.log('connected to database')
 }).catch(function (err) {
   console.log(err)
-})
-
-app.get('/', (req, res) => {
-  filePath = BUILD_PATH + '/index.html'
-
-  if (fs.existsSync(filePath)) {
-    res.sendFile(filePath)
-  }
-  else {
-    res.statusCode = 404
-    res.write('404 not found')
-    res.end()
-  }
 })
 
 const productRoutes = require('./routes/productRoutes')
