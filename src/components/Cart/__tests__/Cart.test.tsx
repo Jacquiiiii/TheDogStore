d6263fa05abf873a9c130f3295aac43a3be9426d
Jacquiiii <jacqui.koroll@telus.com>
2023-09-22 14:14:56 -0700

    feat: add cart tests

--- src/components/Cart/__tests__/Cart.test.tsx
+++ src/components/Cart/__tests__/Cart.test.tsx
@@ -1,4 +1,6 @@
 import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { act } from 'react-dom/test-utils'
 import { BrowserRouter } from 'react-router-dom'
 import { renderWithProviders } from '../../../utils/utils-for-tests'
 import { cartData } from '../../../__mocks__/data/cartData'
@@ -42,7 +44,7 @@ describe('Cart', () => {
     expect(screen.getByText('Subtotal: $68.41')).toBeInTheDocument()
   })
 
-  it('renders a checkout button when there are items in the cart', () => {
+  it('renders a checkout button when there are items in the cart and user is logged in', () => {
     const initialState: any = { login: { isLoggedIn: true, userId: '1' }, cart: cartData.cart }
 
     renderWithProviders(
@@ -55,7 +57,25 @@ describe('Cart', () => {
     expect(screen.getByRole('button', { name: 'Checkout' })).toBeInTheDocument()
   })
 
-  it('renders a login to checkout button when there are items in the cart', () => {
+  it('directs to checkout page when checkout button is clicked', () => {
+    const initialState: any = { login: { isLoggedIn: true, userId: '1' }, cart: cartData.cart }
+
+    renderWithProviders(
+      <BrowserRouter>
+        <Cart />
+      </BrowserRouter>,
+      { initialState }
+    )
+  
+    expect(screen.getByRole('button', { name: 'Checkout' })).toBeInTheDocument()
+
+    act(() => {
+      userEvent.click(screen.getByRole('button', { name: 'Checkout' }))
+      expect(window.location.pathname).toBe('/Checkout')
+    })
+  })
+
+  it('renders a login to checkout button when there are items in the cart and user is not logged in', () => {
     const initialState: any = { login: { isLoggedIn: false, userId: '' }, cart: cartData.cart }
 
     renderWithProviders(
@@ -66,5 +86,23 @@ describe('Cart', () => {
     )
   
     expect(screen.getByRole('button', { name: 'Login to proceed to checkout' })).toBeInTheDocument()
+  })
+
+  it('directs to login page when login to checkout button is clicked', () => {
+    const initialState: any = { login: { isLoggedIn: false, userId: '' }, cart: cartData.cart }
+
+    renderWithProviders(
+      <BrowserRouter>
+        <Cart />
+      </BrowserRouter>,
+      { initialState }
+    )
+  
+    expect(screen.getByRole('button', { name: 'Login to proceed to checkout' })).toBeInTheDocument()
+    
+    act(() => {
+      userEvent.click(screen.getByRole('button', { name: 'Login to proceed to checkout' }))
+      expect(window.location.pathname).toBe('/Login')
+    })
   })
 })
